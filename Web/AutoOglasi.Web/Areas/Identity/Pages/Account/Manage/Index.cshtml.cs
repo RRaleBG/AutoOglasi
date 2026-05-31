27eaf37fac54dbcb066053efdbf8410098395560
Rados Rajcic <168780292+RRaleBG@users.noreply.github.com>
2026-05-31 05:54:46 +0200

    Update Index.cshtml.cs

--- Web/AutoOglasi.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Web/AutoOglasi.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,1 +1,17 @@
 
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microsoft.AspNetCore.Identity.UI.Pages.Account.Manage.Internal
+{
+    public class IndexModel : PageModel
+    {
+        // Ova klasa ostaje prazna jer će Azure u pozadini 
+        // povući ugrađenu logiku, ali nam je potreban ovaj fajl
+        // kako bi Razor kompajler uspešno povezao @model direktivu.
+    }
+}
